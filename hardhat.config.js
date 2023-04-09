96eb090cb786bad4049ead43ab6788e0fa49b6f3
BarryBCW <wangb87@student.ubc.ca>
2023-04-08 18:21:37 -0700

    deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,23 @@
+
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+
+    matic: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.MATIC_PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY
+    }
+  }
+
 };
